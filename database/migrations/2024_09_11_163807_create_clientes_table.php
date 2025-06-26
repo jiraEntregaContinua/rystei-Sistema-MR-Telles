3f23852af3ce860dea2c9cf253629a1b6272acff
el1ziane <eliziane.erb@gmail.com>
2025-06-26 20:42:36 -0300

    editacao de migration

--- database/migrations/2024_09_11_163807_create_clientes_table.php
+++ database/migrations/2024_09_11_163807_create_clientes_table.php
@@ -15,15 +15,6 @@ return new class extends Migration
             $table->string('telefone', 20);
             $table->timestamps(); // created_at e updated_at
         });
-        Schema::create('parcelas', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('cliente_id')->constrained()->onDelete('cascade');
-            $table->string('parcela_numero');
-            $table->decimal('valor', 10, 2);
-            $table->date('data_vencimento');
-            $table->string('status_pagamento')->default('pendente');
-            $table->timestamps();
-        });
         
     }
     
